e4cc4353a414fe355211c38147c0bc9f396904f1
skalantech <escacarlos1982@gmail.com>
2024-01-13 11:38:56 -0500

    added luaCpp

---
+++ luaCpp/src/main.cpp
@@ -0,0 +1,22 @@
+#include <numeric>
+#include <iostream>
+#include <sol/sol.hpp>
+
+
+void TestLua() {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    lua.script_file("./src/script.lua");
+
+    // This is how we get the value of a global variable from our Lua script
+    int someVariableInsideCpp = lua["some_variable"];
+    std::cout << "The value of some_variable in C++ is " << someVariableInsideCpp << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    
+    TestLua();
+
+    return 0;
+}
---
+++ luaCpp/src/script.lua
@@ -0,0 +1,2 @@
+-- This is a Lua global variable
+some_variable = 3^(5 * 2)
